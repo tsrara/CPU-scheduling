45117311dc877e66b8871681dc1391e0a9ae5ebb
Jina <jina@Jinaui-MacBook-Pro.local>
2015-05-21 01:39:02 +0900

    Initial Commit

---
+++ simulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simulator
+//
+//  Created by Jina on 2015. 5. 21..
+//  Copyright (c) 2015년 Jina. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
